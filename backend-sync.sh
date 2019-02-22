780cefb5fc79f6e7c4c27670aa3b7615c1553dc7
philipgreat <philip_chang@163.com>
2019-02-22 12:43:25 +0800

    Update to new version

--- backend-sync.sh
+++ backend-sync.sh
@@ -1,7 +1,7 @@
 
 MODELNAME=oms
-cd ~/githome/-biz-suite/&& git pull --allow-unrelated-histories && cd ../
-cd ~/githome/-biz-suite/bizcore&& gradle classes && cd ../../
-cd ~/githome/-biz-suite/ && rsync -avz   bizcore/* philipz@philip.doublechaintech.cn:~/resin-3.1.12/webapps//
-ssh philipz@philip.doublechaintech.cn "mysql -uroot -p0254891276 -h 127.0.0.1 < resin-3.1.12/webapps//WEB-INF/_core_src/META-INF/_mysql.sql"
+cd ~/githome/${MODELNAME}-biz-suite/&& git pull --allow-unrelated-histories && cd ../
+cd ~/githome/${MODELNAME}-biz-suite/bizcore&& gradle classes && cd ../../
+cd ~/githome/${MODELNAME}-biz-suite/ && rsync -avz   bizcore/* philipz@philip.doublechaintech.cn:~/resin-3.1.12/webapps/${MODELNAME}/
+ssh philipz@philip.doublechaintech.cn "mysql -uroot -p0254891276 -h 127.0.0.1 < resin-3.1.12/webapps/${MODELNAME}/WEB-INF/${MODELNAME}_core_src/META-INF/${MODELNAME}_mysql.sql"
 
