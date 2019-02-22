f0ea30a7dddd4b8864f538d431261a856bf26cb2
philipgreat <philip_chang@163.com>
2019-02-22 12:43:25 +0800

    Update to new version

--- java-sync.sh
+++ java-sync.sh
@@ -1,7 +1,7 @@
 
 MODELNAME=oms
-cd ~/githome/-biz-suite/&& git pull --allow-unrelated-histories && cd ../
-cd ~/githome/-biz-suite/bizcore&& gradle classes && cd ../../
-cd ~/githome/-biz-suite/ && rsync -avz   bizcore/* philipz@philip.doublechaintech.cn:~/resin-3.1.12/webapps//
+cd ~/githome/${MODELNAME}-biz-suite/&& git pull --allow-unrelated-histories && cd ../
+cd ~/githome/${MODELNAME}-biz-suite/bizcore&& gradle classes && cd ../../
+cd ~/githome/${MODELNAME}-biz-suite/ && rsync -avz   bizcore/* philipz@philip.doublechaintech.cn:~/resin-3.1.12/webapps/${MODELNAME}/
 
 
